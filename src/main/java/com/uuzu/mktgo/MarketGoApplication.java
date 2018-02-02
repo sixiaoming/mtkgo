ab73fe696561052db56098345ce83c29baa2646e
zhaozhq@youzu.com <zhaozhq@youzu.com>
2018-02-02 17:42:58 +0800

    移除配置中心

--- src/main/java/com/uuzu/mktgo/MarketGoApplication.java
+++ src/main/java/com/uuzu/mktgo/MarketGoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -16,8 +16,8 @@ import java.util.concurrent.TimeUnit;
  * @author zj_pc
  */
 @SpringBootApplication
-@SpringCloudApplication
-@EnableFeignClients
+//@SpringCloudApplication
+//@EnableFeignClients
 @EnableCaching
 public class MarketGoApplication {
 
