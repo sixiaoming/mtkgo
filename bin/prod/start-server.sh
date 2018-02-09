5695ae2b33636548844f0c7631515cdaa74b040a
zhaozhq@youzu.com <zhaozhq@youzu.com>
2018-02-09 17:52:32 +0800

    commit

---
+++ bin/prod/start-server.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+if [ -z "$JAVA_HOME" ] ; then
+	export JAVA_HOME=/usr/local/java
+fi
+
+SCRIPT="$0"
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  # Drop everything prior to ->
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+SERVER_HOME=`dirname "$SCRIPT"`
+SERVER_HOME=`cd "$SERVER_HOME"; pwd`
+export SERVER_HOME
+
+LIBDIR=$SERVER_HOME/lib
+export LIBDIR
+
+CLASSPATH=${CLASSPATH}:${SERVER_HOME}/conf
+for lib in ${LIBDIR}/*.jar
+do
+ CLASSPATH=$CLASSPATH:$lib
+ export CLASSPATH
+done
+
+PATH=${JAVA_HOME}/bin:${PATH}
+export PATH
+
+java=$JAVA_HOME/bin/java
+
+JAVA_OPTS="-Xms1024m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
+export JAVA_OPTS
+
+echo "JAVA_HOME  :$JAVA_HOME"
+echo "SERVER_HOME:$SERVER_HOME"
+echo "CLASSPATH  :$CLASSPATH"
+
+exec $java -cp $CLASSPATH $JAVA_OPTS com.mobeye.app360.MobApp360Application $* > $0.log  2>&1  &
--- src/main/java/com/uuzu/mktgo/MarketGoApplication.java
+++ src/main/java/com/uuzu/mktgo/MarketGoApplication.java
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.SpringCloudApplication;
+//import org.springframework.cloud.client.SpringCloudApplication;
 //import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
